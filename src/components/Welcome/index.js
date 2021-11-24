501b6144fc1d64eec24faa082e694f2c7b95a561
nataliel2514 <natalielopez2514@gmail.com>
2021-11-24 12:30:34 -0600

    added folders, created a simple blog page

---
+++ src/components/Welcome/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+
+function Welcome() {
+    return (
+        <div>
+        <h1>Welcome to DevWoBlog</h1>
+        <p>Latest Articles</p>
+        </div>
+    );
+};
+
+export default Welcome; 
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Welcome from './App';
+import Welcome from './components/Welcome';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
